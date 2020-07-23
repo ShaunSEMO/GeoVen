04e90bb316e930837350728493e4a83c687c4baa
shaundev <tshegofatsosemenya@gmail.com>
2020-07-23 21:50:40 +0200

    Project config and db connection

--- geoven/geoven/settings.py
+++ geoven/geoven/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'geoven.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'geoven_project',
+        'USER': 'rootMain',
+        'PASSWORD': 'Sp!d3rM@nW@sH3r3',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
